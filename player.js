39b6ded3f79228832d29049e9f82dac67c26912a
Mark Howard <markhjr@outlook.com>
2016-03-01 21:13:48 +0000

    added printing of weapon stats

--- player.js
+++ player.js
@@ -14,52 +14,59 @@ var weapons = [
       name: "Crossbow Of Death", 
       range: 30,
       speed: 20,
-      hp: -20
+      hp: -20,
+      stats: "HP: -20, Strength: 0, Mana: 0, Range: +30, Speed: +20 "
     },
     {
       name: "Longsword of Culling",
       str: 40,
-      hp: -10
+      hp: -10,
+      stats: "HP: -10, Strength: +40 , Mana: 0, Range: 0 , Speed: 0 "
     },
     {
       name: "Mighty Longsword",
       str: 50,
-      speed: -10
+      speed: -10,
+      stats: "HP: 0, Strength: +50, Mana: 0, Range: 0, Speed: -10"
     },
     {
       name: "Shortsword of the Noob",
       str: 10,
-      speed: 5
+      speed: 5,
+      stats: "HP: 0, Strength: +10, Mana: 0, Range: 0, Speed: +5 "
     },
     {
 
       name: "Shortsword of the Calling",
       str: 20,
       hp: 20,
-      speed: 20
-
+      speed: 20,
+      stats: "HP: +20, Strength: +20, Mana: 0, Range: 0, Speed: +20"
     },
     {
       name: "Axe of Decimation",
       str: 50,
-      speed: 30
+      speed: 30,
+      stats: "HP: 0, Strength: +50, Mana: 0, Range: 0, Speed: +30"
     },
     {
 
       name: "Wand Of Justice!",
       mana: 10,
-      hp: 25
-
+      hp: 25,
+      stats: "HP: +25 , Strength: 0, Mana: +10, Range: 0, Speed: 0"
     },
     {
       name: "Blessed Soulbound Staff",
       hp: 10,
       mana: 15,
+      stats: "HP: +10 , Strength: 0, Mana: +15, Range:  0, Speed: 0 "
     },
     {
       name: "Wand of Darkest Night",
       hp: -20,
-      mana: 30
+      mana: 30,
+      stats: "HP: -20, Strength: 0, Mana: +30, Range: 0, Speed: 0 "
     }
     
 ];
@@ -89,10 +96,10 @@ function Player(caste, name) {
 var utils = {
   print: function(player) {
     return ("HP: " + player.hp +
-            "<br>Mana: " + player.mana +
             "<br>Strength: " + player.str +
-            "<br>Speed: " + player.speed +
-            "<br>Range: " + player.range + "<br><br>"
+            "<br>Mana: " + player.mana +
+            "<br>Range: " + player.range +
+            "<br>Speed: " + player.speed + "<br><br>"
            );
   },
   charPrint: function(player) {
@@ -105,9 +112,6 @@ var utils = {
             "Range: " + player.range
            );
   },
-  // weaponPrint: function(weapon) {
-  //   return ("Weapon: " + weapon.name);
-  // },
   randomizer: function(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
   },
@@ -130,7 +134,8 @@ var utils = {
     var location = utils.randomizer(locations);
     
     setTimeout(function() {
-      $("#new-stats").append(player.name + " found the " + weaponName + " at the " + location + "!<br><br>");
+      $("#new-stats").append(player.name + " found the " + weaponName + " at the " + location + "!<br>");
+      $("#new-stats").append("Weapon: " + weapon.name + ",<br>Stats: " + weapon.stats);
     }, 2000);
     setTimeout(function() {
       $("#new-stats").append('<h4>Current</h4><hr>');
@@ -163,5 +168,8 @@ $(document).ready(function() {
     $("#new-stats").empty();
     $("#new-stats").append("<p>Exploring...</p>");
     utils.explore(p[0]);
-
-})
+    console.log(p[0]);
+   // utils.weaponPrint(weapon);
+  });
+  
+});
