7658c417bff7ba9cbc87fab946602d2998224ca5
Mark Howard <markhjr@outlook.com>
2016-03-01 21:45:23 +0000

    Added colors

---
+++ everything.js
@@ -0,0 +1,72 @@
+var locations = [
+  "Evergreen Forest", 
+  "Garden of Thall", 
+  "Oblivion Sewer", 
+  "Orbal Dungeon of Death", 
+  "Pool of Solitude", 
+  "Galadril Foundry", 
+  "Ruins of Snake"
+];
+
+var weapons = [
+    
+    {
+      name: "Crossbow Of Death", 
+      range: 30,
+      speed: 20,
+      hp: -20,
+      stats: "HP: -20, Strength: 0, Mana: 0, Range: +30, Speed: +20 "
+    },
+    {
+      name: "Longsword of Culling",
+      str: 40,
+      hp: -10,
+      stats: "HP: -10, Strength: +40 , Mana: 0, Range: 0 , Speed: 0 "
+    },
+    {
+      name: "Mighty Longsword",
+      str: 50,
+      speed: -10,
+      stats: "HP: 0, Strength: +50, Mana: 0, Range: 0, Speed: -10"
+    },
+    {
+      name: "Shortsword of the Noob",
+      str: 10,
+      speed: 5,
+      stats: "HP: 0, Strength: +10, Mana: 0, Range: 0, Speed: +5 "
+    },
+    {
+
+      name: "Shortsword of the Calling",
+      str: 20,
+      hp: 20,
+      speed: 20,
+      stats: "HP: +20, Strength: +20, Mana: 0, Range: 0, Speed: +20"
+    },
+    {
+      name: "Axe of Decimation",
+      str: 50,
+      speed: 30,
+      stats: "HP: 0, Strength: +50, Mana: 0, Range: 0, Speed: +30"
+    },
+    {
+
+      name: "Wand Of Justice!",
+      mana: 10,
+      hp: 25,
+      stats: "HP: +25 , Strength: 0, Mana: +10, Range: 0, Speed: 0"
+    },
+    {
+      name: "Blessed Soulbound Staff",
+      hp: 10,
+      mana: 15,
+      stats: "HP: +10 , Strength: 0, Mana: +15, Range:  0, Speed: 0 "
+    },
+    {
+      name: "Wand of Darkest Night",
+      hp: -20,
+      mana: 30,
+      stats: "HP: -20, Strength: 0, Mana: +30, Range: 0, Speed: 0 "
+    }
+    
+];
--- player.js
+++ player.js
@@ -1,76 +1,3 @@
-var locations = [
-  "Evergreen Forest", 
-  "Garden of Thall", 
-  "Oblivion Sewer", 
-  "Orbal Dungeon of Death", 
-  "Pool of Solitude", 
-  "Galadril Foundry", 
-  "Ruins of Snake"
-];
-
-var weapons = [
-    
-    {
-      name: "Crossbow Of Death", 
-      range: 30,
-      speed: 20,
-      hp: -20,
-      stats: "HP: -20, Strength: 0, Mana: 0, Range: +30, Speed: +20 "
-    },
-    {
-      name: "Longsword of Culling",
-      str: 40,
-      hp: -10,
-      stats: "HP: -10, Strength: +40 , Mana: 0, Range: 0 , Speed: 0 "
-    },
-    {
-      name: "Mighty Longsword",
-      str: 50,
-      speed: -10,
-      stats: "HP: 0, Strength: +50, Mana: 0, Range: 0, Speed: -10"
-    },
-    {
-      name: "Shortsword of the Noob",
-      str: 10,
-      speed: 5,
-      stats: "HP: 0, Strength: +10, Mana: 0, Range: 0, Speed: +5 "
-    },
-    {
-
-      name: "Shortsword of the Calling",
-      str: 20,
-      hp: 20,
-      speed: 20,
-      stats: "HP: +20, Strength: +20, Mana: 0, Range: 0, Speed: +20"
-    },
-    {
-      name: "Axe of Decimation",
-      str: 50,
-      speed: 30,
-      stats: "HP: 0, Strength: +50, Mana: 0, Range: 0, Speed: +30"
-    },
-    {
-
-      name: "Wand Of Justice!",
-      mana: 10,
-      hp: 25,
-      stats: "HP: +25 , Strength: 0, Mana: +10, Range: 0, Speed: 0"
-    },
-    {
-      name: "Blessed Soulbound Staff",
-      hp: 10,
-      mana: 15,
-      stats: "HP: +10 , Strength: 0, Mana: +15, Range:  0, Speed: 0 "
-    },
-    {
-      name: "Wand of Darkest Night",
-      hp: -20,
-      mana: 30,
-      stats: "HP: -20, Strength: 0, Mana: +30, Range: 0, Speed: 0 "
-    }
-    
-];
-
 function Player(caste, name) {
   this.name = name;
   this.hp = 100;
@@ -102,12 +29,12 @@ var utils = {
             "<br>Speed: " + player.speed + "<br><br>"
            );
   },
-  charPrint: function(player) {
+  characterPrint: function(player) {
     return ("Name: " + player.name + "<br>" +
             "Class: " + player.caste + "<br>" +
             "HP: " + player.hp + "<br>" +
-            "Mana: " + player.mana + "<br>" +
             "Strength: " + player.str + "<br>" +
+            "Mana: " + player.mana + "<br>" +
             "Speed: " + player.speed + "<br>" +
             "Range: " + player.range
            );
@@ -134,42 +61,48 @@ var utils = {
     var location = utils.randomizer(locations);
     
     setTimeout(function() {
-      $("#new-stats").append(player.name + " found the " + weaponName + " at the " + location + "!<br>");
-      $("#new-stats").append("Weapon: " + weapon.name + ",<br>Stats: " + weapon.stats);
+      $("#new-stats").append("<h5>" + player.name + " found the " + weaponName + " at the " + location + "!</h5>");
+      $("#new-stats").append("<span>Weapon:</span> " + weapon.name + ",<br><span>Stats:</span> " + weapon.stats + "<br><br>");
     }, 2000);
+    
     setTimeout(function() {
       $("#new-stats").append('<h4>Current</h4><hr>');
       $("#new-stats").append(utils.print(player));
     }, 3000);
+    
     setTimeout(function() {
       utils.newStats(player, weapon);
-      $("#new-stats").append('<h4>New</h4><hr>')
+      $("#new-stats").append('<h4>New</h4><hr>');
       $("#new-stats").append(utils.print(player));
     }, 5000);
   }
 };
 
-var p = [];
+var playerObjectArray = [];
+
 $(document).ready(function() {
+  
   $("#create-player").on("click", function() {
     var name = $("#name").val();
     var caste = $("#caste").val();
     var newPlayer = new Player(caste, name);
-    $("#new-player").hide();
-    $("#char-sheet").show();
-    $("#explore").show();
-    $("#output").append(utils.charPrint(newPlayer));
-    p.push(newPlayer);
-    
+    if(!name) {
+      alert("Name is required");
+    } else {
+      $("#new-player").hide();
+      $("#char-sheet").show();
+      $("#explore").show();
+      $("#output").append(utils.characterPrint(newPlayer));
+      playerObjectArray.push(newPlayer);
+    }
   });
-
-
+  
   $("#explore").on("click", function() {
     $("#new-stats").empty();
     $("#new-stats").append("<p>Exploring...</p>");
-    utils.explore(p[0]);
-    console.log(p[0]);
-   // utils.weaponPrint(weapon);
+    utils.explore(playerObjectArray[0]);
+    $("#output").empty();
+    $("#output").append(utils.characterPrint(playerObjectArray[0]));
   });
   
 });
